47e4111f27cb3c71f18cf945df1a820504109ed9
anil babu <anilbabu3245@gmail.com>
2023-12-28 13:21:54 +0530

    project intial

---
+++ src/Components/Lending/Home/HomeLending.jsx
@@ -0,0 +1,10 @@
+
+import React from 'react'
+import Styles from './HomeLending.module.css'
+const HomeLending = () => {
+  return (
+    <div>HomeLending</div>
+  )
+}
+
+export default HomeLending
